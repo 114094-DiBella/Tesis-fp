fd9499c92be639e9cb3b84db9f563c99764bf892
agustin <114094@tecnicatura.frc.utn.edu.ar>
2025-05-25 20:30:24 -0300

    Commit inicicial

---
+++ src/main/java/tesis/tesisfp/TesisFpApplication.java
@@ -0,0 +1,13 @@
+package tesis.tesisfp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesisFpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TesisFpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tesis/tesisfp/TesisFpApplicationTests.java
@@ -0,0 +1,13 @@
+package tesis.tesisfp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesisFpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
